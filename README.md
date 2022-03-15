7f24a9a217d9890b5f36a3967ee51d2cd22cc451
Masayuki Uehara <munky69rock@gmail.com>
2022-03-15 11:03:22 +0900

    feat: update to latest

--- README.md
+++ README.md
@@ -4,15 +4,21 @@
 
 1. Google Formのメニューからスクリプトエディタを選択
 2. スクリプトIDをコピーし、`clasp clone`を実行
-  ```sh
-  $ clasp clone "1jlfdnYmV0L_...." --rootDir=./src
-  $ rm ./src/コード.js
-  ```
+
+    ```sh
+    clasp clone "1jlfdnYmV0L_...." --rootDir=./src
+    rm ./src/コード.js
+    ```
+
 3. スクリプト編集画面の「ファイル > プロジェクトのプロパティ > スクリプトのプロパティ」に `TITLE`(Slack通知時のタイトル) と `SLACK_URL`(Webhook URL) を設定
 4. `clasp push`を実行
-  ```sh
-  $ clasp push
-  ```
+
+    ```sh
+    clasp push
+    ```
+
+    - もしエラーになる場合、 `.clasp.json` がプロジェクトルートにあるか確認する (なければmvする)
+
 5. スクリプト編集画面の「編集 > 現在のプロジェクトのトリガー」をクリック、新しいトリガーを作成する
 6. 実行する関数に `onFormSubmit`、イベントの種類を選択に`フォーム送信時`を設定し保存
 7. 権限を許可する
--- src/main.ts
+++ src/main.ts
@@ -3,43 +3,46 @@ const SLACK_URL = scriptProperty.getProperty("SLACK_URL");
 const TITLE = scriptProperty.getProperty("TITLE");
 
 // https://developers.google.com/apps-script/guides/triggers/events
-interface IFormEvent {
-    authMode: GoogleAppsScript.Script.AuthMode;
-    response: GoogleAppsScript.Forms.FormResponse;
-    source: GoogleAppsScript.Forms.Form;
-    triggerUid: string;
+interface FormEvent {
+  authMode: GoogleAppsScript.Script.AuthMode;
+  response: GoogleAppsScript.Forms.FormResponse;
+  source: GoogleAppsScript.Forms.Form;
+  triggerUid: string;
 }
 
-interface IAttachment {
-    title?: string;
-    pretext?: string;
-    text: string;
+interface Attachment {
+  title?: string;
+  pretext?: string;
+  text: string;
 }
 
-function sendToSlack(url: string, data: {}) {
-    UrlFetchApp.fetch(url, {
-        method: "post",
-        contentType: "application/json",
-        payload: JSON.stringify(data)
+function sendToSlack(
+  url: string,
+  data: { text: string; attachments: Attachment[] }
+) {
+  UrlFetchApp.fetch(url, {
+    method: "post",
+    contentType: "application/json",
+    payload: JSON.stringify(data),
+  });
+}
+
+function onFormSubmit(e: FormEvent) {
+  const formResponse = e.response;
+  const itemResponses = formResponse.getItemResponses();
+  const attachments: Attachment[] = [];
+
+  itemResponses.forEach((itemResponse) => {
+    const title = itemResponse.getItem().getTitle();
+    const response = itemResponse.getResponse();
+    attachments.push({
+      title,
+      text: response.toString(),
     });
-}
+  });
 
-function onFormSubmit(e: IFormEvent) {
-    const formResponse = e.response;
-    const itemReponses = formResponse.getItemResponses();
-    const attachments: IAttachment[] = [];
-
-    itemReponses.forEach(itemResponse => {
-        const title = itemResponse.getItem().getTitle();
-        const response = itemResponse.getResponse();
-        attachments.push({
-            title,
-            text: response.toString()
-        })
-    });
-
-    sendToSlack(SLACK_URL, {
-        text: TITLE,
-        attachments
-    })
+  sendToSlack(SLACK_URL, {
+    text: TITLE,
+    attachments,
+  });
 }
